3e6d6374bec9b3914201649a221b95f9a467ff02
dute0987 <dute0987@gmail.com>
2022-03-20 17:28:31 +0530

    Vishal Dute- Expection file

---
+++ CabinvoiceExpection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInoviceGenarator
+{
+    public class CabInoviceException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            NULL_RIDES,
+            INVALID_USER_ID,
+        }
+        ExceptionType type;
+        public CabInoviceException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
